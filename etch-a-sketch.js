c439f1525fbf0416b671622a14068373ad6db210
Arthyr M <arthyrxd@hotmail.de>
2021-10-15 16:14:45 +0200

    Create the worst possible way of making an entire single box

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,22 @@
-const container = document.querySelector("#container");
+function boxes() {
+  for (let i = 0; i < 256; i++) {
+    const container = document.querySelector("#container");
 
-const box = document.createElement("div");
-box.textContent = "box";
+    const box = document.createElement("div");
+    box.textContent = "box";
 
-container.appendChild(box);
+    container.appendChild(box);
+  }
+}
 
-const box1 = document.createElement("div");
-box1.textContent = "box";
+boxes();
 
-container.appendChild(box1);
+// const box1 = document.createElement("div");
+// box1.textContent = "box";
 
-const box2 = document.createElement("div");
-box2.textContent = "box";
+// container.appendChild(box1);
 
-container.appendChild(box2);
+// const box2 = document.createElement("div");
+// box2.textContent = "box";
+
+// container.appendChild(box2);
