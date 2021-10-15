271281bb69abae09b06309c7d01d9d313c102176
Arthyr M <arthyrxd@hotmail.de>
2021-10-15 15:08:54 +0200

    Create more boxes in JS and give them some styling

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,16 @@
 const container = document.querySelector("#container");
 
+const box = document.createElement("div");
+box.textContent = "box";
+
+container.appendChild(box);
+
 const box1 = document.createElement("div");
-box1.textContent = "Hello World!";
+box1.textContent = "box";
 
 container.appendChild(box1);
+
+const box2 = document.createElement("div");
+box2.textContent = "box";
+
+container.appendChild(box2);
