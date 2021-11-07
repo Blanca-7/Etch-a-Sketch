0b589151663bff073c2fbf28e2e5d5312e3cdb79
Arthyr M <arthyrxd@hotmail.de>
2021-11-07 18:57:50 +0100

    Create function for colorTrail effect & create first set of boxes with user prompt

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,6 @@
 // creates 16x16 = 256 boxes total
-function createBoxes() {
-  for (let i = 0; i < 256; i++) {
+function createBoxes(userValue) {
+  for (let i = 0; i < userValue; i++) {
     const container = document.querySelector("#container");
 
     const box = document.createElement("div");
@@ -10,7 +10,8 @@ function createBoxes() {
   }
 }
 
-createBoxes();
+createBoxes(prompt("Enter a number of boxes to create"));
+coloredBoxes();
 
 // deletes set of boxes
 function deleteBoxes() {
@@ -18,25 +19,27 @@ function deleteBoxes() {
   boxes.forEach((box) => {
     box.remove();
   });
-
-  // const box = document.querySelectorAll(".new");
-  // for (let i = 0; i < box.length; i++) {
-  //   box.remove();
-  // }
 }
+// const box = document.querySelectorAll(".new");
+// for (let i = 0; i < box.length; i++) {
+//   box.remove();
+// }
 
-deleteBoxes();
+// deleteBoxes();
 
 // upon mouseenter changes each box to a static color
-const singleBox = document.querySelectorAll(".new");
 
-singleBox.forEach((sBox) => {
-  sBox.addEventListener("mouseenter", () => {
-    sBox.classList.add("colorTrail");
+function coloredBoxes() {
+  const singleBox = document.querySelectorAll(".new");
+
+  singleBox.forEach((sBox) => {
+    sBox.addEventListener("mouseenter", () => {
+      sBox.classList.add("colorTrail");
+    });
   });
-});
-
-// button for reseting
+}
+// button for reseting/clear
+const singleBox = document.querySelectorAll(".new");
 const reset = document.querySelector("#reset-btn");
 
 singleBox.forEach((sBox) => {
@@ -49,14 +52,14 @@ singleBox.forEach((sBox) => {
 
 const user = document.querySelector("#prompt-btn");
 
-let userValue;
-
 user.addEventListener("click", () => {
   let userValue = prompt(
     "Please enter a number less than 100 to generate a new set of tiles"
   );
   if (userValue > 0 && userValue <= 100) {
-    createBoxes();
+    deleteBoxes(); // bug persists, when creating a new set of tiles with prompt, unable to clear colors
+    createBoxes(userValue);
+    coloredBoxes();
   } else {
     alert("Please Enter a number between 1 and 100");
   }
